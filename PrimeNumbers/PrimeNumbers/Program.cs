33265ae1ddc817416a0b272838a0dea738b59ef4
VQ-works <homeworky43@gmail.com>
2025-07-08 21:48:01 +0400

    Add files via upload

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PrimeNumbers;
+
+class Program
+{
+    static int x, y;
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 1000; i++)
+        {
+            if (i != x)
+            {
+                if (i % x == 0)
+                {
+                    y++;
+                }
+                i--;
+            }
+            else
+            {
+                if (y == 1)
+                {
+                    Console.WriteLine(i);
+
+                }
+                y = 0;
+                x = 0;
+            }
+            x++;
+
+        }
+    }
+}
+
+
